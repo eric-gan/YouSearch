50bc82a293da384685deef30e2327a856a53c6b3
Eric Gan <gandalf@amazon.com>
2019-07-26 16:03:31 -0700

    still need to debug upload

--- transcribe_audio.py
+++ transcribe_audio.py
@@ -1,11 +1,34 @@
 import boto3
+import time
 
+BUCKET = 'yousearchdev'
 ACCESS_KEY = 'AKIA4TOXGMUPL2ID4ENZ'
 SECRET_ACCESS_KEY = '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'
-s3 = boto3.client('transcribe', aws_access_key_id=ACCESS_KEY, aws_secret_access_key=SECRET_ACCESS_KEY)
+REGION_NAME = 'us-east-2'
+transcribe = boto3.client('transcribe', aws_access_key_id=ACCESS_KEY, 
+    aws_secret_access_key=SECRET_ACCESS_KEY, region_name=REGION_NAME)
 
-def transcribe(link):
+def transcribe_file(link):
     v_id = link.split('=')[-1]
+    job_name = v_id
+    job_uri = 'https://' + BUCKET + '.s3.us-east-2.amazonaws.com/' + v_id + '.mp4'
+    print(job_uri)
+    transcribe.start_transcription_job(
+        TranscriptionJobName=job_name,
+        Media={'MediaFileUri': job_uri},
+        MediaFormat='mp4',
+        LanguageCode='en-US',
+        OutputBucketName=BUCKET,
+    )
+    while True:
+        status = transcribe.get_transcription_job(TranscriptionJobName=job_name)
+        if status['TranscriptionJob']['TranscriptionJobStatus'] in ['COMPLETED', 'FAILED']:
+            break
+        print("Not ready yet...")
+        time.sleep(5)
+    print(status)
+
+transcribe_file('https://www.youtube.com/watch?v=lzkKzZmRZk8')
 
 
 
--- youtube_to_s3.py
+++ youtube_to_s3.py
@@ -2,21 +2,24 @@ from pytube import YouTube
 
 import boto3
 import botocore.vendored.requests.packages.urllib3 as urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 ACCESS_KEY = 'AKIA4TOXGMUPL2ID4ENZ'
 SECRET_ACCESS_KEY = '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'
-s3 = boto3.client('s3', aws_access_key_id=ACCESS_KEY, aws_secret_access_key=SECRET_ACCESS_KEY)
+BUCKET = 'yousearchdev'
+REGION_NAME = 'us-east-2'
+s3 = boto3.client('s3', aws_access_key_id=ACCESS_KEY, 
+    aws_secret_access_key=SECRET_ACCESS_KEY, region_name=REGION_NAME)
 
 def download_video(link):
     v_id = link.split('=')[-1]
     yt = YouTube(link)
     stream = yt.streams.filter(progressive=True, file_extension="mp4").all()[-1]
-
-    bucket = 'yousearchdev'
+    print(stream.url)
     key = v_id + '.mp4'
 
     http = urllib3.PoolManager()
-    s3.upload_fileobj(http.request('GET', stream.url, preload_content=False), bucket, key)
+    s3.upload_fileobj(http.request('GET', stream.url, preload_content=False), BUCKET, key)
     return
 
-print(download_video('https://www.youtube.com/watch?v=lzkKzZmRZk8'))
+print(download_video('https://www.youtube.com/watch?v=NsKaCS3CtsY'))
