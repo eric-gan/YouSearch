1f4e02ff87dde6265afbf6a7c248d74523a8a33e
Eric Gan <gandalf@amazon.com>
2019-07-31 17:35:42 -0700

    got youtube working

--- main.py
+++ main.py
@@ -2,9 +2,24 @@ from flask import Flask, request, render_template, redirect, url_for
 import os, threading, json
 
 app = Flask(__name__)
-@app.route("/", methods=["GET"])
+@app.route('/', methods=['GET'])
 def homepage():
-    return render_template("index.html")
+    print('loaded homepage')
+    return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def get_video():
+    print('dklfjal;skdjf')
+    video_input = request.form['search']
+    print(video_input)
+    return results(video=video_input)
+
+@app.route('/results/')
+def results(video=None):
+    prefix = 'https://www.youtube.com/embed/'
+    video_tag = video.split('=')[1]
+    video_embed = prefix + video_tag
+    return render_template('results.html', video=video_embed)
 
 if __name__ == "__main__":
     app.run(debug=True)
